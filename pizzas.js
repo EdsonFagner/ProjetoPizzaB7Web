9abc05f3994a9935f0031a2f981a6b3271231c06
Edson Fagner <edson.fagner.souto@gmail.com>
2021-11-29 00:06:53 -0300

    Projeto Pizza

---
+++ pizzas.js
@@ -0,0 +1,9 @@
+let pizzaJson = [
+    {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Molho de tomate, mussarela e oregano.'},
+    {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, azeitona verde, calabresa, cebola e oregano.'},
+    {id:3, name:'Quatro Queijos', img:'images/pizza3.png', price:17.45, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, catupiry, provolone, prato e oregano.'},
+    {id:4, name:'Americana', img:'images/pizza4.png', price:19.77, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, azeitona preta, tomate em rodela, palmito, pimentão e oregano.'},
+    {id:5, name:'Sorvete', img:'images/pizza5.png', price:21.43, sizes:['320g', '530g', '860g'], description:'Massa especial de chocolate, sorvete de baunilha, cauda de chocolate e chocolate em pedaços.'},
+    {id:6, name:'Moda da Casa', img:'images/pizza6.png', price:18.55, sizes:['320g', '530g', '860g'], description:'Molho de tomate, mussarela, azeitona verde e preta, tomate em rodela e oregano.'},
+    {id:7, name:'Chocolate', img:'images/pizza7.png', price:22.36, sizes:['320g', '530g', '860g'], description:'Chocolate derretido e confetes.'}
+];
